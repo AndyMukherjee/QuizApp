c0daef9a552ada0c1f2464df1c1c038e6d526a9d
Deepanjan Roy <deepanjan.roy@gmail.com>
2015-03-25 20:01:07 -0400

    test

--- QuizApp.js
+++ QuizApp.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+var correct = "Your Answere Is Correct";
+var wrong =" Your Answere is incorrect";
+var A1 =="Maternal Grand Uncle"
+var A = $('input').on('click', function(event){
 
-})
+	if (A == A1){
+		{$('.one').append(correct)};
+	
+	else {$('.one').append(wrong+'')};
+};
+});
+
+
+});
 
