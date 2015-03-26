2cf130174f65a96905690f6a391b63585ebdd8ad
Deepanjan Roy <deepanjan.roy@gmail.com>
2015-03-25 20:33:58 -0400

    test

--- QuizApp.js
+++ QuizApp.js
@@ -1,17 +1,39 @@
 $(document).ready(function(){
 
+	var s = 0;
+
 var correct = "Your Answere Is Correct";
-var wrong =" Your Answere is incorrect";
-var A1 =="Maternal Grand Uncle"
-var A = $('input').on('click', function(event){
+var wrong =" Your Answere is incorrect . The Correct Answere is :";
+var A1 =="Maternal Grand Uncle";
+var A2 =="Clarabelle Cow";
+var A3 =="Periwinkle";
+var A4 =="Melody";
+var A5 =="Step-brothers";
+var A6 =="Inventer";
+var A7 =="Isabella";
+var A8 =="Arendelle";
+var A9 =="Bear";
+var A10 =="Eugene Fitzherbert";
 
-	if (A == A1){
-		{$('.one').append(correct)};
+$('.one').(function(){
+
+$('#B2').click(function(){
+var A = $('input').val();
+
+	if (A == A1)
+		{$('.one').append(correct);
+		s=s+1;};
 	
-	else {$('.one').append(wrong+'')};
-};
-});
+	else {$('.one').append(wrong+'A1')};
+
+});//b2 function closes
+
+$('#B1').click(function(){
+	$(.'one').hide();
+	$(.'two').show();
+});//B1 function closes
+}); //$one function closes
 
 
-});
+});// document ready function closes
 
